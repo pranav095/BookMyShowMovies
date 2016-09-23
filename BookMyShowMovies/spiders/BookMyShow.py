f9f5312933e7ce7661263135efd9ca422bff9695
Amit Kumar <amitkmr@users.noreply.github.com>
2016-09-23 12:22:19 +0530

    spider to crawl bookmyshow in delhi NCR region, extorting movie details

--- BookMyShowMovies/spiders/BookMyShow.py
+++ BookMyShowMovies/spiders/BookMyShow.py
@@ -1,13 +1,24 @@
-# -*- coding: utf-8 -*-
+
 import scrapy
+import json
+from scrapy.selector import Selector
+from BookMyShowMovies.items import BookmyshowmoviesItem
 
 
 class BookmyshowSpider(scrapy.Spider):
-    name = "BookMyShow"
+    name = "BookMyShowSpider"
     allowed_domains = ["in.bookmyshow.com"]
     start_urls = (
-        'http://www.in.bookmyshow.com/',
+        'https://in.bookmyshow.com/national-capital-region-ncr/movies',
     )
 
     def parse(self, response):
-        pass
+        movies_dict = []
+        movies = response.xpath('//div[@class="wow fadeIn movie-card-container"]').extract()
+        for movie in movies:
+            mov = BookmyshowmoviesItem()
+            mov['name'] = Selector(text=movie).xpath('//div[@class="detail"]//a[@class="__movie-name"]/text()').extract()
+            mov['languages'] = Selector(text=movie).xpath('//div[@class="detail"]//div[@class="languages"]//li/text()').extract()
+            mov['genre'] = Selector(text=movie).xpath('//div[@class="detail"]//div[@class="genre-list"]//a/div/text()').extract()
+            # mov['rating'] = Selector(text=movie).xpath('//div[@class="stats-wrapper"]//div[@class="__percentage"]/text()').extract()
+            yield mov
