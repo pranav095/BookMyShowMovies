7b9bd042d78b72ac5e53e01da266941d8d9b8aa5
Amit Kumar <amitkmr@users.noreply.github.com>
2016-09-23 12:18:21 +0530

    Defined custom item with properties of a movie

--- BookMyShowMovies/items.py
+++ BookMyShowMovies/items.py
@@ -10,5 +10,7 @@ import scrapy
 
 class BookmyshowmoviesItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    languages = scrapy.Field()
+    genre = scrapy.Field()
+    # rating = scrapy.Field()
